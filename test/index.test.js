8d4850465cca4cc4c326f1d1eb0c159afa41ac48
Nikhil Verma <nikhilve99@gmail.com>
2026-09-22 07:00:13 +0000

    test: demonstrate CI failure

--- test/index.test.js
+++ test/index.test.js
@@ -3,5 +3,5 @@ const assert = require("node:assert/strict");
 const { getGreeting } = require("../src/index.js");
 
 test("getGreeting returns the GitHub Actions greeting", () => {
-  assert.equal(getGreeting(), "Hello from GitHub Actions");
+  assert.equal(getGreeting(), "Hello from GitHub Actions.");
 });
