2833eb8fac11b50c8e2cf0cbd6e5bf1be88f6c75
Nikhil Verma <nikhilve99@gmail.com>
2026-09-22 08:05:52 +0000

    testing branch

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,11 @@
 const test = require("node:test");
 const assert = require("node:assert/strict");
-const { getGreeting } = require("../src/index.js");
+const { getGreeting, getVersion } = require("../src/index.js");
 
 test("getGreeting returns the GitHub Actions greeting", () => {
   assert.equal(getGreeting(), "Hello from GitHub Actions.");
+});
+
+test("getVersion returns the project version", () => {
+  assert.equal(getVersion(), "1.0.0");
 });
